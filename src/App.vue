e6f01a65264c81b235417f0c0a301e357018bd6e
Victoria <vmbobey@gmail.com>
2021-10-02 07:49:53 -0600

    Setup 3

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,10 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
+@import "./assets/css/colors.scss";
+
 *,
 *::before,
 *::after {
@@ -30,18 +33,10 @@ export default {
 
 html {
   font-size: 62.5%;
+  font-family: "Space Mono", monospace;
 }
 
 body {
   box-sizing: border-box;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>Search Bar</div>
+  <form @submit.prevent="searchUser">
+    <input
+      id="username"
+      v-model.trim="username"
+      type="text"
+      placeholder="Search GitHub username..."
+    />
+    <button :disabled="!username" type="submit">Search</button>
+  </form>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+    };
+  },
+  methods: {
+    async searchUser() {
+      if (this.username) {
+        const response = await axios.get(
+          `https://api.github.com/users/${this.username}`
+        );
+        console.log(response);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
