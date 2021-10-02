107d30aa30d36a18b374cdc18205fd1fc8540d9a
Victoria <vmbobey@gmail.com>
2021-10-01 18:23:10 -0600

    Initial commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header></the-header>
+  <search-bar></search-bar>
+  <user-profile></user-profile>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader.vue";
+import SearchBar from "./components/SearchBar.vue";
+import UserProfile from "./components/UserProfile.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+    SearchBar,
+    UserProfile,
+  },
+};
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <h1>devfinder</h1>
+    <button>Dark</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
