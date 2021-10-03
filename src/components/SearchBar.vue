c9490a251e0b5a6e4f592e02b479afbccad526aa
Victoria <vmbobey@gmail.com>
2021-10-03 14:39:14 -0600

    SearchBar initial styling
    Base styles, does not include interactive states

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -40,4 +40,37 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+form {
+  width: 100%;
+  height: 7rem;
+  background-color: var(--color-content-background);
+  border-radius: 1.5rem;
+  padding: 2rem 1rem;
+  display: flex;
+  align-items: center;
+}
+
+input {
+  width: 100%;
+  color: var(--color-text);
+  font-size: 1.8rem;
+  font-family: inherit;
+  border: none;
+  background-color: transparent;
+  padding: 0 2rem;
+}
+
+button {
+  height: 5rem;
+  width: 10.6rem;
+  color: var(--color-white);
+  font-size: 1.6rem;
+  font-family: inherit;
+  font-weight: 700;
+  background-color: var(--color-blue);
+  border: none;
+  border-radius: 1rem;
+  margin-left: auto;
+}
+</style>
