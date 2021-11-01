2c51bb103cfea9079b3f2655fa038ad8b8d1e07e
Victoria <vmbobey@gmail.com>
2021-10-31 19:54:02 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/user-search-app' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/user-search-app/' : '/',
   css: {
     loaderOptions: {
       sass: {
